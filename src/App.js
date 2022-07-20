21bfc40fabdd9394c5a6ecf9c56cc105aa4e3ae7
denizyesill <denizyesil2000@gmail.com>
2022-07-20 16:58:04 +0300

    input and title update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const App = () => {
     universite:'',
     bolum:'',
     pozisyon:'',
+    code:'',
+    code1:'',
+    code2:'',
+    code3:'',
     languages:'',
     tecrube:'',
     referans:'',
@@ -39,217 +43,201 @@ const App = () => {
   return (
     <div className="page_container">
 
-      
+
       <img src={bitesLogo}  width="272px" height="82px"/>
+
 
       <div className="form_section">
         <form onSubmit={handleSubmit} className="form_container">
          
 
-           <form> 
-        <div className="form-wrapper">
-        <label className="form-label">Başlık</label>
-        <input className="form-input" 
-        type="text"
-        name="baslık"
-        value={inputs.baslık || ""}
-        onChange={handleChange}
-        />
-        </div>
 
-      </form>  
+  {/* <form> 
+        <div className="form-wrapper">
+          <label className="form-label">Başlık</label>
+           <input className="form-input" 
+             type="text"
+             name="baslık"
+             value={inputs.baslık || ""}
+             onChange={handleChange}
+            />
+         </div>
+       </form>   */}
          
          
-          <legend>
-            <h3>Kişisel Bilgiler</h3>
-          </legend>
-          <br />
+
+          <label className="alt-baslıklar">
+            Kişisel Bilgiler
+          </label>
+          
+          
         
-        <div className="username">
-         <label className="form-label">Ad-Soyad</label>
-            {/* <h5>Ad-Soyad:</h5>  */}
-            <input className="username-input"
+         <div className="form-wrapper">
+          <label className="form-label">Ad-Soyad:</label>
+            <input className="form-input"
               type="text"
               value={inputs.username}
               onChange={(e)=>{handleChange('username',e.target.value)}}
             />
-          
          </div>
-        
          
 
-        <div className="tc">
-        <label>
-            <h5>T.C. Numarası: </h5>
-            <input
+       <div className="form-wrapper">
+          <label className="form-label">T.C. Numarası:</label>
+            <input className="form-input-number"
               type="number"
               value={inputs.tc}
               onChange={(e)=>{handleChange('tc',e.target.value)}}
             />
-          </label>
         </div>
 
 
-         <div className="birthday">
-         <label>
-            <h5>Doğum Tarihi: </h5>
-            <input
+       <div className="form-wrapper">
+          <label className="form-label">Doğum Tarihi:</label>
+            <input className="form-input-date"
               type="date"
               value={inputs.birthday}
               onChange={(e)=>{handleChange('birthday',e.target.value)}}
             />
-          </label>
          </div>
 
           
-          <div className="cinsiyet">
-          <label>
-          <h5>Cinsiyet: </h5>
-              <select 
+         <div className="form-wrapper">
+           <label className="form-label">Cinsiyet:</label>
+             <select className="form-input-cinsiyet"
               value={inputs.cinsiyet}
               onChange={(e)=>{handleChange('cinsiyet',e.target.value)}}>
                 <option value="erkek">erkek</option>
                 <option value="kadın">Kadın</option>
                 
               </select>
-            </label>
           </div>
 
 
-        <div className="adress">
-        <label>
-            <h5>Adress:</h5>
-            <input
+       <div className="form-wrapper">
+         <label className="form-label">Adress:</label>
+          <input className="form-input"
             type="text"
             value={inputs.adress}
             onChange={(e)=>{handleChange('adress',e.target.value)}}
             />
-            </label>
         </div>
 
 
-        <legend>
-         <h3>İletişim Bilgileri</h3>
-        </legend>
+        <label className="alt-baslıklar">
+          İletişim Bilgileri
+        </label>
 
-       <div className="mail">
-       <label>
-            <h5>Email:</h5>
-            <input
+
+      <div className="form-wrapper">
+         <label className="form-label">Email:</label>
+            <input className="form-input-email"
               type="email"
               value={inputs.mail}
               onChange={(e)=>{handleChange('mail',e.target.value)}}
             />
-          </label>
        </div>
            
 
-           <div className="telno">
-          <label>
-            <h5>Telefon Numarası: </h5>
-            <input
+      <div className="form-wrapper">
+         <label className="form-label">Telefon Numarası:</label>
+            <input className="form-input-number"
               type="number"
               value={inputs.telno}
               onChange={(e)=>{handleChange('telno',e.target.value)}}
             />
-          </label>
-           </div>
+        </div>
 
 
-        <legend>
-         <h3>Eğitim Bilgileri</h3>
-        </legend>
+        <label className="alt-baslıklar">
+          Eğitim Bilgileri
+        </label>
 
-      <div className="universite">
-      <label>
-            <h5>Üniversite:</h5>
-            <input
+     <div className="form-wrapper">
+        <label className="form-label">Üniversite:</label>
+           <input className="form-input"
             type="text"
             value={inputs.universite}
             onChange={(e)=>{handleChange('universite',e.target.value)}}
             />
-         </label>
       </div>
 
 
-        <div className="bolum">
-        <label>
-            <h5>Bölüm:</h5>
-           <input
+      <div className="form-wrapper">
+       <label className="form-label">Bölüm:</label>
+         <input className="form-input"
             type="text"
             value={inputs.bolum}
             onChange={(e)=>{handleChange('bolum',e.target.value)}}
             />
-         </label>
         </div>
 
 
 
-        <legend>
-         <h3>Başvuru Yapılan Pozisyon</h3>
-        </legend>
+        <label className="alt-baslıklar">
+          Başvuru Yapılan Pozisyon
+        </label>
             
-           <div className="pozisyon">
-           <label>
-              <select 
+
+        <div className="form-wrapper">
+           <label> </label>
+             <select className="form-input-pozisyon"
               value={inputs.pozisyon}
               onChange={(e)=>{handleChange('pozisyon',e.target.value)}}>
                 <option value="ik">İK</option>
                 <option value="erpuzmanı">ERP Uzmanı</option>
                 
               </select>
-            </label>
            </div>
 
 
 
-        <legend>
-         <h3>Bilgisayar Bilgisi/Yabancı Dil</h3>
-        </legend>
+        <label className="alt-baslıklar">
+          Bilgisayar Bilgisi/Yabancı Dil
+        </label>
+
        
-       <div className="checkbox-wrapper">
-       <label className="my-label">Python
-       <input type="checkbox"         onChange={(e)=>{handleChange('code',e.target.value)}}  value="python" />
-        </label>
+      <div className="checkbox-wrapper">
 
-        <label className="my-label">C
-       <input type="checkbox"        onChange={(e)=>{handleChange('code1',e.target.value)}}   value="c" />
-        </label>
+         <label className="my-label">Python
+           <input type="checkbox"         onChange={(e)=>{handleChange('code',e.target.value)}}  value="python" />
+         </label>
 
-        <label className="my-label">C++
-       <input type="checkbox"         onChange={(e)=>{handleChange('code2',e.target.value)}}   value="c++" />
-        </label>
+          <label className="my-label">C
+            <input type="checkbox"        onChange={(e)=>{handleChange('code1',e.target.value)}}   value="c" />
+          </label>
 
-        <label className="my-label">Javascript
-       <input type="checkbox"        onChange={(e)=>{handleChange('code3',e.target.value)}}   value="javascript" />
-        </label>
+          <label className="my-label">C++
+            <input type="checkbox"         onChange={(e)=>{handleChange('code2',e.target.value)}}   value="c++" />
+          </label>
+
+          <label className="my-label">Javascript
+            <input type="checkbox"        onChange={(e)=>{handleChange('code3',e.target.value)}}   value="javascript" />
+          </label>
 
        </div>
        
       
 
-       <div className="languages">
-       <label>
-            <h5>Yabancı Dil:</h5>
-            <input
+      <div className="form-wrapper">
+        <label className="form-label">Yabancı Dil:</label>
+          <input className="form-input"
             type="text"
             value={inputs.languages}
             onChange={(e)=>{handleChange('languages',e.target.value)}}
-            />
-            </label>
+            />  
        </div>
         
 
 
+        <label className="alt-baslıklar">
+          Tecrübeler
+        </label>
 
-        <legend>
-         <h3>Tecrübeler</h3>
-        </legend>
 
-
-      <div className="tecrube">
-      <label>
-            <input
+      <div className="form-wrapper">
+        <label>
+          <input className="form-input"
             type="text"
             value={inputs.tecrube}
             onChange={(e)=>{handleChange('tecrube',e.target.value)}}
@@ -257,13 +245,15 @@ const App = () => {
          </label>
       </div>
 
-        <legend>
-         <h3>Referanslar</h3>
-        </legend>
 
-      <div className="referans">
-      <label>
-            <input
+        <label className="alt-baslıklar">
+          Referanslar
+        </label>
+
+
+      <div className="form-wrapper">
+        <label>
+          <input className="form-input"
             type="text"
             value={inputs.referans}
             onChange={(e)=>{handleChange('referans',e.target.value)}}
@@ -271,31 +261,29 @@ const App = () => {
          </label>
       </div>
 
-         <legend>
-         <h3>Diğer Bilgiler</h3>
-        </legend>
 
-        <div className="basarı">
-        <label>
-        <h5>Başarılar/Ödüller:</h5>
-            <input
+         <label className="alt-baslıklar">
+           Diğer Bilgiler
+         </label>
+
+
+      <div className="form-wrapper">
+        <label className="form-label">Başarılar/Ödüller:</label>
+          <input className="form-input"
             type="text"
             value={inputs.basarı}
             onChange={(e)=>{handleChange('basarı',e.target.value)}}
-            />
-         </label>
-        </div>
+            /> 
+      </div>
 
 
-       <div className="hobi">
-       <label>
-        <h5>Hobiler:</h5>
-            <input
+      <div className="form-wrapper">
+        <label className="form-label">Hobiler:</label>
+          <input className="form-input"
             type="text"
             value={inputs.hobi}
             onChange={(e)=>{handleChange('hobi',e.target.value)}}
-            />
-         </label>
+            />        
        </div>
           
 
